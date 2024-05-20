aa70dc14412b9ecf8a7baeb5c26b37651769f5a7
lbaculima <128737741+CapuccinoFroth@users.noreply.github.com>
2024-05-20 15:57:06 +0200

    Update route.ts

--- app/api/tx/route.ts
+++ app/api/tx/route.ts
@@ -28,8 +28,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
     params: {
       abi: [],
       data,
-      //to: BUY_MY_COFFEE_CONTRACT_ADDR,
-      to: "0x5acd432e02BD6BAfE819b044FcCf60F278C7b8E1", //Lore's nft contract address
+      to: BUY_MY_COFFEE_CONTRACT_ADDR,
+     // to: "0x5acd432e02BD6BAfE819b044FcCf60F278C7b8E1", //Lore's nft contract address
       value: parseEther('0.00004').toString(), // 0.00004 ETH
     },
   };
