ddc5837488e6588c1ad9b3724ca3192b46728023
lbaculima <128737741+CapuccinoFroth@users.noreply.github.com>
2024-05-20 13:50:55 +0200

    test
    test

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ import { NEXT_PUBLIC_URL } from './config';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: 'Story time',
+      label: 'Story time1',
     },
     {
       action: 'tx',
-      label: 'Send Base Sepolia',
+      label: 'Send Base Sepolia1',
       target: `${NEXT_PUBLIC_URL}/api/tx`,
       postUrl: `${NEXT_PUBLIC_URL}/api/tx-success`,
     },
@@ -25,10 +25,10 @@ const frameMetadata = getFrameMetadata({
 });
 
 export const metadata: Metadata = {
-  title: 'zizzamia.xyz',
+  title: 'ziz.xyz',
   description: 'LFG',
   openGraph: {
-    title: 'zizzamia.xyz',
+    title: 'Berlin Base Frame project',
     description: 'LFG',
     images: [`${NEXT_PUBLIC_URL}/park-1.png`],
   },
@@ -40,7 +40,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <h1>zizzamia.xyz</h1>
+      <h1>Berlin Base Frame project1</h1>
     </>
   );
 }
