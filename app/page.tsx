0a218a70819dfa7af270659801249ea518e64c11
lbaculima <128737741+CapuccinoFroth@users.noreply.github.com>
2024-05-20 17:45:50 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,13 @@ const frameMetadata = getFrameMetadata({
       label: 'Story time2',
     },*/
     /*{
-      action: 'mint',
-      label: 'test mint',
+      action: 'link',
+      label: 'Go Mint you access!',r
       target: 'https://testnet.holograph.xyz/open-edition/joey-collins-requiem-by-joey-collins-uk',
     },*/
     {
-      action: 'link',
-      label: 'Go Mint your access!',
+      action: 'mint',
+      label: 'Mint test',
       target: 'https://testnets.opensea.io/assets/base-sepolia/0xf9f9755cb214ba584866f976fe83a0e467692bb5/1',
     },
   ],
